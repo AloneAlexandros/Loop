f4d612ed86e51c0ca4deaffc3029652547b6a792
Alexandros Samonakis <98485800+AloneAlexandros@users.noreply.github.com>
2025-08-03 00:09:58 +0300

    new wall types, three new levels and logo/text

---
+++ Assets/Scripts/Killer.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Killer : MonoBehaviour
+{
+    public Movethatcamera movethatcamera;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Player?")
+        {
+            movethatcamera.ResetMap();
+        }
+    }
+}
